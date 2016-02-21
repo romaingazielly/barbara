37b70f5d1792b208b8ba32ee8fbee3211bf625bb
Romain <romain.gazielly@gmail.com>
2016-02-21 20:04:22 +0100

    Home inté

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
+var $window = $(window),
+	$h = $window.height(),
+	$w = $window.width();
+
 jQuery(document).ready(function($) {
+
+	// Windows
+	resizeHome();
+	$(window).resize(function(){ 
+		resizeHome();
+	}); 
+
+
+	// Burger
 	$('.burger').on('click', function(){
 		$('.burger').toggleClass('btn-open');
 	})
 });
+
+function resizeHome() {
+	if (document.all) {
+		$h=document.body.clientHeight;
+		$w=document.body.clientWidth;
+	} else {
+		$w=window.innerWidth;
+		$h=window.innerHeight;
+	}
+	console.log('window: '+$h, $w);
+	$('.home').css({ 'height': $h + 'px' }); // Header
+}
