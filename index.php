dd9006de63e039c7c9515e6ff10d16650250d8f9
Romain <romain.gazielly@gmail.com>
2018-02-18 12:15:51 +0400

    Header fix

--- index.php
+++ index.php
@@ -22,7 +22,43 @@
     </head>
 
     <body class="page-home">
-      <?php include('partials/header.php'); ?>
+      <header>
+        <div class="left">
+
+          <div class="sub-head">
+            <!-- Logo -->
+            <a href="/" id="logo" title="Barbara Sevin">
+              <img src="/images/logo.png" alt="Barbara Sevin" width="144" height="19" srcset="/images/logo-x2.png 2x">
+              <span>Branding & Digital ideas</span>
+            </a>
+
+            <!-- Nav -->
+            <div class="burger"><a href="javascript:;" class="btn-burger" title="Menu" menu-button=""><span>Menu</span></a></div>
+          </div>
+          <nav>
+            <div class="vert-center">
+              <div>
+              <p><a href="">Projects</a></p>
+              <ul>
+                <li><a href="/projets/animaux-sante" title="Projet Animaux">Animaux Santé</a></li>
+                <li><a href="/projets/saupiquet" title="Projet Saupiquet">Saupiquet</a></li>
+                <li><a href="/projets/balle-au-bond" title="Projet La balle au bond">La Balle au Bond</a></li>
+                <li><a href="/projets/le-petit-marseillais" title="Projet Le Petit Marseillais">Le Petit Marseillais</a></li>
+                <li><a href="/projets/auzou" title="Projet Auzou Éditions">Auzou Éditions</a></li>
+                <li><a href="/projets/classic-expert" title="Projet Classic Expert">Classic Expert</a></li>
+                <li><a href="/projets/igloo" title="L'igloo Immobilier">L'igloo Immobilier</a></li>
+                <li><a href="/projets/wilkinson-summer" title="Wilkinson Summer">Wilkinson Summer</a></li>
+              </ul>
+              <p><a href="/moodboards" title="Moodboards">Moodboards</a></p>
+              <p><a href="/about.html" title="About">About</a></p>
+              </div>
+            </div>
+          </nav>
+        </div>
+        <div class="right">
+          
+        </div>
+      </header>
 
       <section class="container home">
         <article class="projet" id="balle-au-bond">
